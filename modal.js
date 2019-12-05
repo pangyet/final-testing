1f5891234e792a9004cfda97511ca6705e1b2980
pangyet <58503931+pangyet@users.noreply.github.com>
2019-12-05 15:01:16 +0800

    added files

---
+++ modal.js
@@ -0,0 +1,14 @@
+ 
+
+ var strings = [
+    "deta",
+
+    "deatera",
+    
+    "details3"
+  ]
+
+  function showModal(index){
+    $("div.modal-content div.modal-body").text(strings[index]);
+  }
+
